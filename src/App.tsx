d52aed7e3252135095361d5a85474aab58e89208
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-07-13 16:35:09 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-
-
+import {Dashboard} from "./components/ui/Dashboard";
+import {AuthProvider} from "./contexts/AuthContext.tsx";
+import {ProtectedRoute} from "./components/auth/protectedRoute.tsx";
 function App() {
   return (
-    <div className='bg-gray-100 flex flex-col items-center justify-center w-full'>
-        hey
-    </div>
+    <AuthProvider>
+        <ProtectedRoute>
+        <Dashboard/>
+        </ProtectedRoute>
+    </AuthProvider>
   )
 }
 
