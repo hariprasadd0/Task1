3fb222334982b679058cac04620a81074fe3d121
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-07-14 00:03:08 +0530

    fix: add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import {Dashboard} from "./components/ui/Dashboard";
 import {AuthProvider} from "./contexts/AuthContext.tsx";
 import {ProtectedRoute} from "./components/auth/protectedRoute.tsx";
+import {Layout} from './components/layout/Layout.tsx'
 function App() {
   return (
     <AuthProvider>
         <ProtectedRoute>
+            <Layout>
         <Dashboard/>
+            </Layout>
         </ProtectedRoute>
     </AuthProvider>
   )
